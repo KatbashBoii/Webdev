e2a3474a226f0a3012dde9436b724698e9da2e7e
KatbashBoii <kirotheassassin@gmail.com>
2025-10-19 12:07:57 +0400

    fixing php WIP

--- phps/register.php
+++ phps/register.php
@@ -1,5 +1,21 @@
 <?php
-
-    print_r($_POST);
-
-    ?>
+if ($_POST) {
+    $fName = $_POST["firstName"] ?? null;
+    $lName = $_POST["lastName"] ?? null;
+    $tel = $_POST["phoneNumber"] ?? null;
+    $email = $_POST["email"] ?? null;
+    $password = $_POST["password"] ?? null;
+    
+    // Validatation and sanitizing data
+    $fName = htmlspecialchars(trim($fName));
+    $lName = htmlspecialchars(trim($lName));
+    $tel = filter_var($tel, FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    
+    var_dump($fName, $lName, $tel, $email, $password);
+    
+    if ($password) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    }
+}
+?>
