22880c7269e4589da3fc496c751f651934aef6e9
KatbashBoii <kirotheassassin@gmail.com>
2025-10-22 20:55:44 +0400

    changes

---
+++ register.php
@@ -0,0 +1,41 @@
+<?php include 'databaseconnect.php'; ?>
+
+<?php
+session_start();
+
+    $fName = $_POST["firstName"] ?? null;
+    $lName = $_POST["lastName"] ?? null;
+    $tel = $_POST["phoneNumber"] ?? null;
+    $email = $_POST["email"] ?? null;
+    $userPassword = $_POST["password"] ?? null;
+    
+    // Validatation and sanitizing data
+    $fName = htmlspecialchars(trim($fName));
+    $lName = htmlspecialchars(trim($lName));
+    $tel = filter_var($tel, FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    
+    if ($userPassword) {
+        $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+    }
+
+    $sql = "INSERT INTO customertable (Fname, Lname, Phone, Email, Password)
+            VALUES (?, ?, ?, ?, ?)";
+
+    $stmt = mysqli_stmt_init($connection);
+
+    if (! mysqli_stmt_prepare($stmt, $sql)) {
+        die(mysqli_error($connection));
+    }
+
+    mysqli_stmt_bind_param($stmt, "sssss",
+                            $fName,
+                            $lName,
+                            $tel,
+                            $email,
+                            $hashedPassword);
+
+    mysqli_stmt_execute($stmt);
+
+    echo "<script>window.location.href = '../login.html';</script>";
+?>
